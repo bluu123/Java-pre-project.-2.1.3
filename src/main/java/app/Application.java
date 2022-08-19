cbbd3f7551192d47deecb7f552ff9e1b149875c9
Bluu123 <79271872363rus@gmail.com>
2022-08-19 18:55:04 +0300

    init

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -8,13 +8,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
     public static void main(String[] args) {
+        long time;
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+            time = bean.getTimer().getTime();
+            System.out.println((time == bean.getTimer().getTime()) + "\n");
         }
+
     }
 
 }
